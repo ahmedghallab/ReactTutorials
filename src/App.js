2661403d05ef517eaf27a500b2a93e45500a1a4d
Ahmed Ghallab <aghallab@uwm.edu>
2020-04-24 16:55:38 -0500

    Lists and conditionals

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '0', name: 'Max', age: 28 },
+      { id: 'asdsa1', name: 'Manu', age: 29 },
+      { id: 'asdsa2', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value'
   }
@@ -20,18 +20,40 @@ class App extends Component {
         { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
-      ]
+      ],
+      otherState: 'different val',
+      showPersons: false
     })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    //copy
+    const person = { ...this.state.persons[personIndex] }
+
+    //now mutating a copy
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons });
+  }
+
+  //this syntax is usually better for a method, so
+  // this keyword is referencing the class
+  togglePersons = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow }) // 
+  }
+
+
+  deletePersons = (personIndex) => {
+    const person = [...this.state.persons];// fethc all the persons, but create a copy
+    // by spreading out all the persons array to a list of elements
+    person.splice(personIndex, 1);// splice first person
+    this.setState({ persons: person })
   }
   render() {
     const style = {
@@ -41,7 +63,26 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
-
+    let persons = null
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            // mapping the .state.persons array to 
+            // jsx object array, that react will pull out
+            this.state.persons.map((person, index) => {
+              return <Person
+                click={() => this.deletePersons(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id} //it's good to have this for react to know what to change in the ui
+                change={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            })
+          }
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -51,19 +92,8 @@ class App extends Component {
         However, BIND is better and more efficient*/}
         <button
           style={style}
-          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          //notic this click passed as props to Person
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          onClick={this.togglePersons}>Switch Name</button>
+        {persons}{/*  clean way */}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
