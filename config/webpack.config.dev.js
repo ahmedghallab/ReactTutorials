16aa842667a48eb3dd925050fefd31c112e68262
Ahmed Ghallab <aghallab@uwm.edu>
2020-04-28 10:58:16 -0500

    Css modules from styled components
    //vid 73
    splitting css modules, from JS, which is a better approach

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -165,7 +165,7 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                   modules:true,
-                  localIdentName: '[name]__[local]__[hash:base64:5]'//vid 73
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
               },
               {
