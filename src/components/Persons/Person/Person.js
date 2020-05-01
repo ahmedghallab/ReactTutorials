dc09dfb353c9287b1c46b004650c858f82a9fd4d
Ahmed Ghallab <aghallab@uwm.edu>
2020-05-01 14:20:05 -0500

    Using ref
    Refs in classes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,22 +3,32 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {
-  render() {
-    console.log('[Person.js] rendering...');
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          value={this.props.name}
-        />
-      </div>
-    );
-  }
+    constructor(props) {
+        super();
+        this.inputElementRef = React.createRef();
+    }
+    componentDidMount() {
+        // this.inputElement.focus(); alt way
+        this.inputElementRef.current.focus();
+    }
+    render() {
+        console.log('[Person.js] rendering...');
+        return (
+            <div className={classes.Person} >
+                <p onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input
+                    type="text"
+                    onChange={this.props.changed}
+                    // ref={(inputEl) => { this.inputElement = inputEl }} alt way
+                    ref={this.inputElementRef}//behind the scence react can connect the dots
+                    value={this.props.name}
+                />
+            </div>
+        );
+    }
 }
 
-export default withClass(Person,classes.Person);
+
+export default withClass(Person, classes.Person);
