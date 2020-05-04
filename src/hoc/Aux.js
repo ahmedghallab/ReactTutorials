def677fd81be243556b67cd555a10e377f915ed4
Ahmed Ghallab <aghallab@uwm.edu>
2020-05-04 10:17:10 -0500

    fixed some issues
    reset the project to stop issues from popping up

--- src/hoc/Aux.js
+++
@@ -1,6 +0,0 @@
-
-// children is espcial property that simply
-// outputs whatever gets entered bet the opening and 
-//closing tag of this component
-const aux = props => props => props.children;
-export default aux;
